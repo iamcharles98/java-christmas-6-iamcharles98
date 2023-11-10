8cbfc1418576d7d3ffd57e77f65bfcee590df219
chansoo <chansoo38@gmail.com>
2023-11-10 18:13:25 +0900

    <feat> MainMenu - 메인 메뉴 Enum 을 생성한다.

---
+++ src/main/java/christmas/Domain/MainMenu.java
@@ -0,0 +1,25 @@
+package christmas.Domain;
+
+public enum MainMenu {
+    T_BONE_STEAK("티본스테이크", 55000),
+    LIB("바비큐립", 54000),
+    SEAFOOD_PASTA("해산물파스타", 35000),
+    CHRISTMAS_PASTA("크리스마스파스타", 25000);
+
+    private String name;
+    private int price;
+
+    MainMenu(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
