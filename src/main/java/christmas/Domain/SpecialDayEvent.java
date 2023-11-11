0ee6fd7f707ba8a49a7088f7d3b54b8e0f80a268
chansoo <chansoo38@gmail.com>
2023-11-11 17:23:15 +0900

    <feat> SpecialDayEvent - 특별 할인 이벤트 객체를 생성한다.

---
+++ src/main/java/christmas/Domain/SpecialDayEvent.java
@@ -0,0 +1,25 @@
+package christmas.Domain;
+
+import java.util.List;
+
+public class SpecialDayEvent extends Event {
+    private final int DISCOUNT_UNIT = 1000;
+    private final List<Integer> eventDay = List.of(
+            3,
+            10,
+            17,
+            24, 25,
+            31);
+
+    @Override
+    public boolean isEventDay(int day) {
+        return eventDay.contains(day);
+    }
+
+    @Override
+    public void apply(Reservation reservation) {
+        if (super.canMeetCondition(reservation.getTotalPrice())) {
+            int discountMoney = DISCOUNT_UNIT;
+        }
+    }
+}
