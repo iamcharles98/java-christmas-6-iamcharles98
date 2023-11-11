e5384d4b9155249c4c57923b52be9e471ca6a751
chansoo <chansoo38@gmail.com>
2023-11-11 15:50:27 +0900

    <feat> Constants - 공통적으로 쓰일 상수를 가지는 클래스를 생성한다.

---
+++ src/main/java/christmas/Constants.java
@@ -0,0 +1,10 @@
+package christmas;
+
+public class Constants {
+    public static final int MIN_AMOUNT = 1;
+    public static final int MAX_AMOUNT = 20;
+    public static final int FIRST_DATE = 1;
+    public static final int LAST_DATE = 31;
+    public static final String LINE = "\n";
+    public static final String SPACE = " ";
+}
