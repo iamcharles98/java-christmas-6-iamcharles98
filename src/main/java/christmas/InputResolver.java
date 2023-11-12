92f09057b2a03e562ff8fbfba28d792ecadfc3fd
chansoo <chansoo38@gmail.com>
2023-11-12 16:55:26 +0900

    <feat> InputResolver - 주문받은 음식이 메뉴판에 존재하는 음식인지 검사 후 주문 내역을 Map 자료구조에 담아 반환하는 기능을 구현한다.

---
+++ src/main/java/christmas/InputResolver.java
@@ -0,0 +1,36 @@
+package christmas;
+
+import static christmas.Constants.AMOUNT_INDEX;
+import static christmas.Constants.NAME_AND_AMOUNT_DELIMITER;
+import static christmas.Constants.NAME_INDEX;
+
+import christmas.Domain.Menu;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class InputResolver {
+
+    private InputResolver() {
+    }
+
+    public static Map<Menu, Integer> stringToOrderMap(String[] inputMenu)
+            throws IllegalArgumentException {
+        final Map<Menu, Integer> orders = new HashMap<>();
+        for (String token : inputMenu) {
+            addIfPresentMenu(orders, token);
+        }
+        return orders;
+    }
+
+    private static void addIfPresentMenu(Map<Menu, Integer> orders, String token) throws IllegalArgumentException {
+        String[] order = token.split(NAME_AND_AMOUNT_DELIMITER);
+        Optional<Menu> optionalMenu = Menu.getMenuByNameIfPresent(order[NAME_INDEX]);
+        if (optionalMenu.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        orders.put(optionalMenu.get(), Integer.parseInt(order[AMOUNT_INDEX]));
+    }
+
+
+}
