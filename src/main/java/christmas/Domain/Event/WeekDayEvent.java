dbb9e4131807fde6e37ed0b45a574b3caf821a44
chansoo <chansoo38@gmail.com>
2023-11-12 23:13:09 +0900

    <fix> WeekDayEvent - 31일이 포함 안되어 있던 버그 수정

--- src/main/java/christmas/Domain/Event/WeekDayEvent.java
+++ src/main/java/christmas/Domain/Event/WeekDayEvent.java
@@ -12,7 +12,7 @@ public class WeekDayEvent extends Event {
             3, 4, 5, 6, 7,
             10, 11, 12, 13, 14,
             17, 18, 19, 20, 21,
-            24, 25, 26, 27, 28);
+            24, 25, 26, 27, 28, 31);
 
     public WeekDayEvent() {
     }
