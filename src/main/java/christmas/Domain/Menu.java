641a80b7f74357e2cdb308a728c2198ed57ba56c
chansoo <chansoo38@gmail.com>
2023-11-12 23:15:18 +0900

    <chore> Menu 타입 별 공백으로 분리

--- src/main/java/christmas/Domain/Menu.java
+++ src/main/java/christmas/Domain/Menu.java
@@ -8,12 +8,18 @@ public enum Menu {
     MUSHROOM_SOUP(MenuType.APPETIZER, "양송이수프", 6000),
     TAPAS(MenuType.APPETIZER, "타파스", 5500),
     CAESAR_SALAD(MenuType.APPETIZER, "시저샐러드", 8000),
+
+
     T_BONE_STEAK(MenuType.MAIN, "티본스테이크", 55000),
     LIB(MenuType.MAIN, "바비큐립", 54000),
     SEAFOOD_PASTA(MenuType.MAIN, "해산물파스타", 35000),
     CHRISTMAS_PASTA(MenuType.MAIN, "크리스마스파스타", 25000),
+
+
     CHOCO_CAKE(MenuType.DESSERT, "초코케이크", 15000),
     ICE_CREAM(MenuType.DESSERT, "아이스크림", 5000),
+
+
     ZERO_COLA(MenuType.DRINK, "제로콜라", 3000),
     RED_WINE(MenuType.DRINK, "레드와인", 60000),
     CHAMPAGNE(MenuType.DRINK, "샴페인", 25000);
