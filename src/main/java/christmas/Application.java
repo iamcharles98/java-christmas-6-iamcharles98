c7a3c92eb033f4c04cfed6eed85636d31c1754bf
chansoo <chansoo38@gmail.com>
2023-11-12 17:58:46 +0900

    <feat> Application - 메인 기능 구현한다.

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,13 @@
 package christmas;
 
+import christmas.Controller.Controller;
+
 public class Application {
+    static Controller controller = Controller.getInstance();
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        controller.init();
+        controller.makeReservationProcess();
+        controller.showCheckOutProcess();
     }
 }
