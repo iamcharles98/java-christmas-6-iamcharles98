1fe2177ada530c51baa650f6ec1f8b4dcec19e21
chansoo <chansoo38@gmail.com>
2023-11-12 17:57:53 +0900

    <feat> CheckOut - 예약 객체 반환하는 기능 추가

--- src/main/java/christmas/Domain/CheckOut.java
+++ src/main/java/christmas/Domain/CheckOut.java
@@ -14,6 +14,10 @@ public class CheckOut {
         choiceBadge();
     }
 
+    public Reservation getReservation() {
+        return reservation;
+    }
+
     public int getAmountOfBenefit() {
         int benefitAmount = 0;
         for (Benefit benefit : benefits) {
