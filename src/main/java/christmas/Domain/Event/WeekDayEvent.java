828647f25401cb3c3adaf7348d95b1e006fc0d07
chansoo <chansoo38@gmail.com>
2023-11-12 17:55:39 +0900

    <fix> WeekDayEvent - 일요일이 평일에 포함되어있지 않았던 버그를 수정한다.

--- src/main/java/christmas/Domain/Event/WeekDayEvent.java
+++ src/main/java/christmas/Domain/Event/WeekDayEvent.java
@@ -9,10 +9,10 @@ public class WeekDayEvent extends Event {
     private final String eventName = "평일 할인";
     private final int DISCOUNT_UNIT = 2023;
     private final List<Integer> eventDay = List.of(
-            4, 5, 6, 7,
-            11, 12, 13, 14,
-            18, 19, 20, 21,
-            25, 26, 27, 28);
+            3, 4, 5, 6, 7,
+            10, 11, 12, 13, 14,
+            17, 18, 19, 20, 21,
+            24, 25, 26, 27, 28);
 
     public WeekDayEvent() {
     }
